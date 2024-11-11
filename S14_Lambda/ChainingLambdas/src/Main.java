4aa862665c4ac8e78274ff98ea08a7aab8962f22
KevinCode98 <kevincode98@gmail.com>
2024-11-10 22:37:35 -0600

    10/11/24

---
+++ S14_Lambda/ChainingLambdas/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Kevin";
+        Function<String, String> uCase = String::toUpperCase;
+        System.out.println(uCase.apply(name));
+
+        Function<String, String> lastName = s -> s.concat(" Carrillo");
+        Function<String, String> uCaseLastName = uCase.andThen(lastName);
+        System.out.println(uCaseLastName.apply(name));
+
+        uCaseLastName = uCase.compose(lastName);
+        System.out.println(uCaseLastName.apply(name));
+
+        Function<String, String[]> f0 = uCase
+                .andThen(s -> s.concat(" Carrillo"))
+                .andThen(s -> s.split(" "));
+
+        System.out.println(Arrays.toString(f0.apply(name)));
+
+        Function<String, String> f1 = uCase
+                .andThen(s -> s.concat(" Carrillo"))
+                .andThen(s -> s.split( " "))
+                .andThen(s -> s[1].toUpperCase() + ", " + s[0]);
+
+        System.out.println(f1.apply(name));
+
+        // Video 194 = - min 5 :c
+    }
+}
--- S14_Lambda/LambdaExpressionChallenge/src/Main.java
+++ S14_Lambda/LambdaExpressionChallenge/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -15,14 +16,22 @@ public class Main {
         List<String> backedByArray = Arrays.asList(names);
 
         System.out.println("--- ADD Random middle initial ---");
-        backedByArray.replaceAll(s -> s += " " + getRandomChar('A', 'D') + "." );
+        backedByArray.replaceAll(s -> s += " " + getRandomChar('A', 'D') + ".");
         System.out.println(Arrays.toString(names));
 
         System.out.println("--- ADD reversed name as last name ---");
         backedByArray.replaceAll(s -> s += " " + getReversedName(s.split(" ")[0]));
         Arrays.asList(names).forEach(n -> System.out.println(n));
 
-        // Stay in min 9:00 in video 190 :c
+        System.out.println("--- REMOVE names where fist = last ---");
+        List<String> newList = new ArrayList<>(List.of(names));
+        newList.removeIf(s -> {
+            String first = s.substring(0, s.indexOf(" "));
+            String last = s.substring(s.lastIndexOf(" ") + 1);
+
+            return first.equals(last);
+        });
+        newList.forEach(n -> System.out.println(n));
     }
 
     public static char getRandomChar(char startChar, char endChar) {
